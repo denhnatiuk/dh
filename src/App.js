f6907cac07b9d500111db81519e87542b6850137
DenysHnatiuk <denys.hnatiuk@gmail.com>
2021-07-08 18:49:13 +0300

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+console.log("hot reload");
+
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const getData=()=>{
+    fetch('https://spreadsheets.google.com/feeds/cells/1EM9Jye2-cDHz8lslK-IykXSIZuaEr9JKsi3vUtKB2Fo/1/public/full?alt=json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +44,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          {
+            data && data.length>0 && data.map((item)=><p>{item.about}</p>)
+          }
         </a>
       </header>
     </div>
